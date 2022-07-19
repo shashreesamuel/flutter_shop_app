8d62b6201249ab3ca6fc14b9fc4c914a32ccfdad
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-07-19 16:56:03 -0400

    Finished working on the listview and getting it close to the dribbble design

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -29,6 +30,21 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey[800],
+        bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: Colors.grey.shade800,
+          buttonBackgroundColor: Colors.grey[700],
+          color: Colors.grey.shade700,
+          animationDuration: Duration(milliseconds: 500),
+          items: [
+            Icon(Icons.arrow_back_ios_new_rounded),
+            Icon(Icons.home_outlined),
+            Icon(Icons.shopping_bag_outlined),
+            Icon(Icons.settings),
+          ],
+          onTap: (index) {
+            //Handle button tap
+          },
+        ),
         body: SafeArea(
           child: Column(
             children: [
@@ -83,33 +99,29 @@ class MyHomePage extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20.0),
                 child: Divider(
-                  color: Colors.grey,
+                  color: Color.fromARGB(66, 255, 253, 253),
                 ),
               ),
               Expanded(
                 child: ListView(
                   children: [
                     Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Container(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
                           child: ClipRRect(
                             borderRadius: BorderRadius.all(Radius.circular(12)),
                             child: Image.network(
-                                'https://images.unsplash.com/photo-1473968512647-3e447244af8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-                            ),
-                          )
-                      ),
-                    ),
+                                'https://images.unsplash.com/photo-1473968512647-3e447244af8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+                          ),
+                        )),
                     Padding(
                       padding: const EdgeInsets.all(16.0),
                       child: Container(
                           child: ClipRRect(
-                            borderRadius: BorderRadius.all(Radius.circular(12)),
-                            child: Image.network(
-                                'https://images.unsplash.com/photo-1508444845599-5c89863b1c44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'
-                            ),
-                          )
-                      ),
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                        child: Image.network(
+                            'https://images.unsplash.com/photo-1508444845599-5c89863b1c44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'),
+                      )),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(16.0),
